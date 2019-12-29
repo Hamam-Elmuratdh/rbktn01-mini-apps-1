9153d552592335633040891d9b2264e84be31dd5
Hamam-Elmuratdh <hamam.alsaiity@gmail.com>
2019-12-29 21:32:56 +0200

    unfinished

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+var express = require('express')
+const bodyParser = require('body-parser')
+const port = 3000
+const app = express()
+app.use(bodyParser.json())
+
+
+var urlencode = bodyParser.urlencoded({ extended: false })
+
+app.post('/uploader/',urlencode, function (req,res){
+  data = req.body
+  console.log(req.body)
+  res.end(" \n\n\n" + data)
+})
+
+app.listen(port, () => console.log(` app listening on port ${port}!`))
 
 
